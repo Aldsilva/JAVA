5e195a703e65d7678d7c6608bd6c1259080ba4cb
Alexsandro <teste@hotmail.com>
2017-11-10 22:17:42 -0200

    Aula3.1

--- cliente-dao/src/br/com/cliente/dao/ClienteDao.java
+++ cliente-dao/src/br/com/cliente/dao/ClienteDao.java
@@ -1,7 +1,7 @@
 package br.com.cliente.dao;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 
 import br.com.cliente.model.Cliente;
 
@@ -9,30 +9,24 @@ public class ClienteDao {
 	
 	private List<Cliente> clientes;
 	
-	public ClienteDao()	{
+	public ClienteDao() {
 		initMock();
 	}
 	
 	private void initMock() {
-		
+	
 		clientes = new ArrayList<Cliente>();
 		
-		/**
-		 * Cliente cliente1 = new Cliente();
-		cliente1.setId(1);
-		cliente1.setNome("Jan");
-		cliente.setIdade(30);
-		cliente1.setSobreNome("Nascimento");
-		cliente1.setTelefone("999999999");
-		 */
+		clientes.add(new Cliente(1, "Daniel", "Barbosa", 30, "99999"));
 		
-		clientes.add(new Cliente(1, "Jandeilson", "Nascimento", 30, "99999"));
-						
 	}
 	
-	public List<Cliente> obterTodosClientes() {
+		public List<Cliente> obterTodosClientes() {
 		return this.clientes;
-		
 	}
-
+	
+	
+	
+	
+	
 }
--- cliente-model/src/br/com/cliente/model/Cliente.java
+++ cliente-model/src/br/com/cliente/model/Cliente.java
@@ -3,96 +3,78 @@ package br.com.cliente.model;
 public class Cliente {
 
 	private int id;
-	
 	private String nome;
-	
 	private String sobreNome;
-	
 	private int idade;
-		
 	private String telefone;
 	
+	
 	public Cliente() {
-				
+		
 	}
 	
-	/**
-	 * Construtor com parametros
-	 */
-	
-	public Cliente(int id, String nome, String sobreNome, int idade, String telefone)	{
+	public Cliente (int id, String nome, String sobreNome, int idade, String telefone) {
+		
 		this.id = id;
 		this.nome = nome;
 		this.sobreNome = sobreNome;
 		this.idade = idade;
 		this.telefone = telefone;
+	}
 		
+	public int getId() {
+		return this.id;
 	}
 	
-    public int getId() {
-    	return this.id;
-    }
-    
-    public void setId(int id) {
-    	this.id = id;
-       	
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 	
-    public String getNome() {
-    	return this.nome;
-        	
-    }
-    
-    public void setNome(String nome) {
-    	this.nome = nome;
-    	    	
-    }
-    
-    public String getSobreNome() {
-		return sobreNome;
+	public String getNome() {
+		return this.nome;
+	}
+	
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	public String getSobreNome() {
+		return this.sobreNome;
 	}
 
 	public void setSobreNome(String sobreNome) {
 		this.sobreNome = sobreNome;
 	}
-    
-    public int getIdade() {
-    	return this.idade;
-    }
-    
-    public void setIdade(int idade) {
-    	this.idade = idade;
-    }
 	
+	public int getIdade() {
+		return this.idade;
+	}
 
-    public String getTelefone() {
-    	return this.telefone;
-        	
-    }
-    
-    public void setTelefone(String telefone) {
-    	this.telefone = telefone;
-    }
-    
-    public String getNomeCompleto() {
-    	
-    	String nomeCompleto = "";
-    	
-    	if(!this.nome.equals("") && !this.sobreNome.equals("")) {
-    		/**
-    		 * return this.nome + " " + this.sobreNome;
-    		 */
-    		nomeCompleto = this.nome + " " + this.sobreNome;
-    		    		
-    	}
-    	return nomeCompleto;
-    	
-    }
-    
-    public String toString() {
-    	return "Nome: " + this.nome + " Sobrenome: " + this.sobreNome + "Idade: " + this.idade + " Telefone: " + this.telefone;
-    	    	
-    }
+	public String getTelefone() {
+		return telefone;
+	}
 
-    
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	
+	public String getNomeCompleto() {
+		
+		String nomeCompleto = "";
+		
+		if(!this.nome.equals("") && !this.sobreNome.equals("")) {
+			nomeCompleto = this.nome + " " + this.sobreNome;
+			}
+		return nomeCompleto;
+	}
+
+	@Override
+	public String toString() {
+		return "Nome: " + this.nome + " Sobrenome: " + this.sobreNome + "Idade: " + this.idade + " Telefone: " + this.telefone;
+	}
 }
+
